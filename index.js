7abe88a2e48c01c242a8f8c00930a14ae0599787
DenisVasconcellos <denis_mvasconcellos@hotmail.com>
2021-07-28 21:42:22 -0300

    tratamento de exceções

--- index.js
+++ index.js
@@ -2,52 +2,82 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const { Tarefa } = require('./models');
- 
+
 app.use(bodyParser.json());
- 
+
 //////////////// Rotas do estado do servidor
- 
+
 app.get('/', (req, res) => {
- res.send('Running... =)');
+    res.send('Running... =)');
 });
- 
-//////////////// Rotas das tarefas
- 
+
+//////////////// Rotas das tarefas 
+
 app.post('/tarefas', async (req, res) => {
- const tarefa = await Tarefa.create(req.body);
- res.json(tarefa);
+    try {
+        const tarefa = await Tarefa.create(req.body);
+        res.status(201).json(tarefa);
+    } catch (error) {
+        res.status(500).json({ message: 'Erro interno no servidor' });
+    }
 });
 
 app.get('/tarefas', async (req, res) => {
-    const tarefas = await Tarefa.findAll();
-    res.json(tarefas);
+    try {
+        const tarefas = await Tarefa.findAll();
+        res.status(200).json(tarefas);
+    } catch (error) {
+        res.status(500).json({ message: 'Erro interno no servidor' });
+    }
+
 });
 
 app.get('/tarefas/:id', async (req, res) => {
-    const tarefa = await Tarefa.findAll({
-        where:{
-            id: req.params.id
+    try {
+        const tarefa = await Tarefa.findAll({
+            where: {
+                id: req.params.id
+            }
+        });
+        if(tarefa.length == 0){
+            res.status(404).json({message: 'Tarefa não encontrada'});
         }
-    });
-    res.json(...tarefa);
+        res.status(200).json(...tarefa);
+    } catch (error) {
+        res.status(500).json({ message: 'Erro interno no servidor' });
+    }
 });
 
-app.delete('/tarefas/:id', async(req, res) => {
-    const result = await Tarefa.destroy({
-        where:{
-            id: req.params.id
+app.delete('/tarefas/:id', async (req, res) => {
+    try {
+        const result = await Tarefa.destroy({
+            where: {
+                id: req.params.id
+            }
+        });
+        if (result == 0) {
+            res.status(404).json({message: 'Tarefa não encontrada'});
         }
-    });
-    res.json(result);
-})
+        res.status(200).json({message: 'Tarefa removida com sucesso'});
+    } catch (error) {
+        res.status(500).json({ message: 'Erro interno no servidor' });
+    }
+});
 
-app.put('/tarefas/:id', async(req, res) => {
-    const tarefa = await Tarefa.update(req.body, {
-        where:{
-            id: req.params.id
+app.put('/tarefas/:id', async (req, res) => {
+    try {
+        const result = await Tarefa.update(req.body, {
+            where: {
+                id: req.params.id
+            }
+        });
+        if(result[0] == 0){
+            res.status(404).json({message: 'Tarefa não encontrada'});
         }
-    });
-    res.json(tarefa);
+        res.status(200).json({message: 'Tarefa atualizada com sucesso'});
+    } catch (error) {
+        res.status(500).json({ message: 'Erro interno no servidor' });
+    }
 });
 
 app.listen(4000);
